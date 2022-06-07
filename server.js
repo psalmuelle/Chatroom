1c12064a79e44992327eddd5252bf831de5cb295
psalmuelle <bjoestores@gmail.com>
2022-06-07 22:56:18 +0100

    chat

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = require("http").createServer(app);
 
 
 const io = require("socket.io")(server);
-app.use(express.static(path.join(__dirname + "/")));
+app.use(express.static(path.join(__dirname + "/path")));
 
 
 io.on("connection", function(socket){
